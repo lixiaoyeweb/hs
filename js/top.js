08c61e3fd38273ecf8f1853bd2cd8326ef55f8a0
lixiaoyeabc <181985715@qq.com>
2017-02-15 13:45:44 +0800

    'hello'

---
+++ js/top.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+window.onload = function(){
+	var obtn = document.getElementById('btn');
+	
+	obtn.onclick = function(){
+		
+			 var osTop = document.documentElement.scrollTop || document.body.scrollTop ;
+			 alert(osTop);
+			 
+			/* document.documentElement.scrollTop = document.body.scrollTop -= 50;
+			 */
+			 }
+	
+	}
